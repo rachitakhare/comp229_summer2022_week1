c3edcfd9be2fdf730ad20bf44b9a3b15a473c22b
rachita <rachitakhare15@gmail.com>
2022-05-09 22:23:49 -0400

    adding solution to teh problem

--- test.js
+++ test.js
@@ -20,6 +20,11 @@ console.log(sum(...myArray));//spread operation
 //Design a function to return multiplication of first two arguments, and sum of the rest 
 
 function sumX(a,b,...args){
+    let numSum = 0;
+    let product = a*b;
+    for(const arg of args){
 
-    
+        numSum+=arg;
+    }
+    return[product,numSum];
 }
